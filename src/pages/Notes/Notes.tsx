2579e073b80c895d0faf8ce3d6c302a02d5d7aff
kami <kami-vi@mail.ru>
2025-07-17 17:36:28 +0400

    feat: implement Notes page

---
+++ src/pages/Notes/Notes.tsx
@@ -0,0 +1,63 @@
+import { useCallback, useState } from 'react'
+import { Box } from '@mui/material'
+import { SearchBox, SideBar, WorkSpace, AuthStatus } from '../../components'
+import type { Note } from '../../types/types'
+
+export const Notes = () => {
+	const [searchQuery, setSearchQuery] = useState('')
+	const [activeNote, setActiveNote] = useState<Note | null>(null)
+	const [refreshTrigger, setRefreshTrigger] = useState({})
+
+	const handleNoteUpdate = useCallback(
+		(updatedNote: Note) => {
+			if (activeNote?.id === updatedNote.id) {
+				setActiveNote(updatedNote)
+			}
+			setRefreshTrigger({})
+		},
+		[activeNote]
+	)
+
+	return (
+		<Box
+			sx={{
+				height: '100vh',
+				width: '1200px',
+				display: 'flex',
+				flexDirection: 'column',
+			}}>
+			<Box
+				sx={{
+					p: 2,
+					borderBottom: '1px solid',
+					borderColor: 'divider',
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignItems: 'center',
+					bgcolor: 'background.paper',
+				}}>
+				<AuthStatus />
+				<SearchBox onSearch={setSearchQuery} />
+			</Box>
+
+			<Box
+				sx={{
+					flexGrow: 1,
+					display: 'grid',
+					gridTemplateColumns: '280px 1fr',
+					bgcolor: 'background.default',
+					overflow: 'hidden',
+				}}>
+				<SideBar
+					searchQuery={searchQuery}
+					setActiveNote={setActiveNote}
+					refreshTrigger={refreshTrigger}
+				/>
+				<WorkSpace
+					activeNote={activeNote}
+					onNoteUpdate={handleNoteUpdate}
+				/>
+			</Box>
+		</Box>
+	)
+}
